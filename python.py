e9cd9f18d176c7bcabb14308b4fb3e12d51073bb
Rahul <rahul999775@gmail.com>
2021-05-06 15:58:03 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,46 @@
+from flask import *
+import os
+from werkzeug.utils import secure_filename
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+
+# Classes 
+classes = { 0:'Benign',
+            1:'Malignant',
+            2:"Normal"}
+
+def image_processing(img):
+    model = load_model('./model/model.h5')
+    data=[]
+    image = Image.open(img)
+    image = image.resize((30,30))
+    data.append(np.array(image))
+    X_test=np.array(data)
+    Y_pred = model.predict_classes(X_test)
+    return Y_pred
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+        file_path = secure_filename(f.filename)
+        f.save(file_path)
+        # Make prediction
+        result = image_processing(file_path)
+        s = [str(i) for i in result]
+        a = int("".join(s))
+        result = "The Ultra Sound Image is : " +classes[a]
+        os.remove(file_path)
+        return result
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
